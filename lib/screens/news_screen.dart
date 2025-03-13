546eef23146480d2ee04893afe5e8358e16ef9bc
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2025-03-13 10:24:17 +0500

    refresh on pull

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -3,8 +3,23 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/logic/bloc/news_bloc.dart';
 import 'package:news_app/widgets/news_card.dart';
 
-class NewsScreen extends StatelessWidget {
+class NewsScreen extends StatefulWidget {
   const NewsScreen({super.key});
+
+  @override
+  State<NewsScreen> createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<NewsBloc>().add(FetchNews("latest"));
+  }
+
+  Future<void> _refreshNews() async {
+    context.read<NewsBloc>().add(FetchNews("latest"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,26 +27,29 @@ class NewsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("The New York Times"),
       ),
-      body: BlocBuilder<NewsBloc, NewsState>(
-        builder: (context, state) {
-          if (state is NewsLoading) {
-            return const Text("Loading...");
-          } else if (state is NewsLoaded) {
-            return ListView.builder(
-              itemCount: state.articles.length,
-              itemBuilder: (context, index) {
-                return NewsCard(article: state.articles[index]);
-              },
+      body: RefreshIndicator(
+        onRefresh: _refreshNews,
+        child: BlocBuilder<NewsBloc, NewsState>(
+          builder: (context, state) {
+            if (state is NewsLoading) {
+              return const Text("Loading...");
+            } else if (state is NewsLoaded) {
+              return ListView.builder(
+                itemCount: state.articles.length,
+                itemBuilder: (context, index) {
+                  return NewsCard(article: state.articles[index]);
+                },
+              );
+            } else if (state is NewsError) {
+              return Center(
+                child: Text(state.message),
+              );
+            }
+            return const Center(
+              child: Text("Search for news"),
             );
-          } else if (state is NewsError) {
-            return Center(
-              child: Text(state.message),
-            );
-          }
-          return const Center(
-            child: Text("Search for news"),
-          );
-        },
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.read<NewsBloc>().add(
